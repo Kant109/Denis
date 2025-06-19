782917470f83be9b9af47666a207341b64ce889c
Quentin ROULET <quentin.roulet@apside.com>
2025-06-19 10:48:30 +0200

    Add score + winner & ranking end tournament

--- src/views/Babykon/BabykonTournament.vue
+++ src/views/Babykon/BabykonTournament.vue
@@ -1,14 +1,104 @@
 <script setup lang="ts">
 import Header from '@/components/Header.vue';
 import { useBabykonStore } from '@/stores/BabykonStore';
+import { computed, onBeforeMount, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { LottieAnimation } from "lottie-web-vue";
+import LoaderAnimation from "../../assets/animations/stats.json";
+
+const router = useRouter();
 
 const babykonStore = useBabykonStore();
 
-const players = babykonStore.players;
+const players = computed(() => babykonStore.players);
+const matchs = computed(() => babykonStore.matchs);
+const startAnimation = ref(false);
+
+function genererMatchs(players: Array<BabykonPlayer>) {
+    let matchId = 0;
+    for (let player = 0; player < players.length; player++) {
+        for (let opponent = player + 1; opponent < players.length; opponent++) {
+            const newMatch = {
+                matchId: matchId++,
+                winnerId: null as number | null,
+                player1: players[player],
+                player2: players[opponent],
+                scoreP1: 0,
+                scoreP2: 0
+            }
+            matchs.value.push(newMatch);
+        }
+    }
+}
+
+function shuffleMatch<T>(array: T[]): T[] {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+const isWinner = (match: BabykonMatch, player: BabykonPlayer) => {
+    match.winnerId = match.winnerId === player.id ? null : player.id;
+}
+
+const isResultsOk = async (): Promise<boolean> => {
+    let isOk = true;
+    matchs.value.forEach(match => {
+        if (match.winnerId === null) {
+            isOk = false;
+        }
+    });
+    return isOk;
+}
+
+const getRanking = async () => {
+    matchs.value.forEach(match => {
+        players.value.forEach(player => {
+            if(match.winnerId === player.id) {
+                player.nbWins += 1;
+                player.score += player === match.player1 ? match.scoreP1 : match.scoreP2;
+            }
+        })
+    })
+    players.value.sort((a, b) => {
+        if (b.nbWins === a.nbWins) {
+            return b.score - a.score;
+        }
+        return b.nbWins - a.nbWins;
+    });
+    babykonStore.players = players.value;
+}
+
+const getResults = async () => {
+    if(await isResultsOk()) {
+        startAnimation.value = true;
+        await getRanking();
+        setTimeout(() => {
+            startAnimation.value = false;
+            router.push({ name: 'babykon-tournament-results' });
+        }, 2000);
+    } else {
+        alert('Veuillez sélectionner un gagnant pour chaque match.');
+    }
+}
+
+onBeforeMount(() => {
+    if(matchs.value.length === 0) {
+        genererMatchs(babykonStore.players);
+        shuffleMatch(matchs.value);
+    } else {
+        players.value.forEach(player => {
+            player.nbWins = 0;
+            player.score = 0;
+        })
+    }
+})
 </script>
 
 <template>
-    <div class="babykon-tournament-container">
+    <div class="babykon-tournament-container" v-if="!startAnimation">
         <div class="header">
             <Header
                 title="Tournoi"
@@ -16,8 +106,179 @@ const players = babykonStore.players;
             />
         </div>
         
+        <div class="matchs-container">
+            <div class="match" v-for="match in matchs" :key="match.matchId">
+                <div class="opposition">
+                    <div class="player" :class="{'winner': match.winnerId === match.player1.id}" @click.prevent="isWinner(match, match.player1)">
+                        <img :src="'https://api.dicebear.com/9.x/adventurer/svg?seed=' + match.player1.firstName + match.player1.pseudo + match.player1.name" alt="Avatar de {{ match.player1.name }}" />
+                        <span>{{ match.player1.pseudo }}</span>
+                    </div>
+                    <div class="vs">VS</div>
+                    <div class="player" :class="{'winner': match.winnerId === match.player2.id}" @click.prevent="isWinner(match, match.player2)">
+                        <img :src="'https://api.dicebear.com/9.x/adventurer/svg?seed=' + match.player2.firstName + match.player2.pseudo + match.player2.name" alt="Avatar de {{ match.player2.name }}" />
+                        <span>{{ match.player2.pseudo }}</span>
+                    </div>
+                </div>
+                <details class="score">
+                    <summary>
+                        <h3 class="see-more">saisie du score</h3>
+                    </summary>
+                    <div class="set-score">
+                        <input type="number" :max="5" :min="-2" v-model="match.scoreP1" :id="match.player1.pseudo" :name="'Score de ' + match.player1.pseudo"/>
+                        <input type="number" :max="5" :min="-2" v-model="match.scoreP2" :id="match.player2.pseudo" :name="'Score de ' + match.player2.pseudo"/>
+                    </div>
+                </details>
+            </div>
+        </div>
+
+        <div class="babykon-tournament-end">
+            <button class="btn" @click.prevent="getResults()">Voir les résultats</button>
+        </div>
     </div>
+    <Teleport to="main" v-else>
+        <dialog class="confirm-end-game-dialog" open>
+            <div class="loader">
+                <LottieAnimation 
+                    :animation-data="LoaderAnimation"
+                    :auto-play="true"
+                    :loop="true"
+                    :speed="1"
+                    ref="anim"
+                    class="anim"
+                />
+            </div>
+        </dialog>
+    </Teleport>
 </template>
 
 <style lang="scss" scoped>
+@import "@/assets/helpers/mixins.scss";
+
+.babykon-tournament-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    background-color: var(--bg-color-primary);
+
+    .header {
+        width: 100%;
+    }
+
+    .matchs-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+        padding: 20px;
+        gap: 1.5rem;
+
+        .match {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-evenly;
+            align-items: center;
+            background-color: white;
+            border-radius: .5rem;
+            width: 100%;
+            border: 1px solid lightgray;
+            
+            .opposition {
+                display: flex;
+                justify-content: space-evenly;
+                align-items: center;
+                width: 100%;
+                padding: .5rem;
+                
+                .player {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    width: 80px;
+                    border: 2px solid transparent;
+                    border-radius: 25%;
+                    
+                    img {
+                        width: 50px;
+                        height: 50px;
+                        border-radius: 50%;
+                    }
+                    
+                    span {
+                        font-family: "Tilt Warp", sans-serif;
+                        margin: 5px 0;
+                    }
+                    
+                    &.winner {
+                        border: 2px solid gold;
+                        border-radius: 25%;
+                    }
+                }
+                .vs {
+                    font-family: "Tilt Warp", sans-serif;
+                    font-size: 24px;
+                    font-weight: bold;
+                }
+            }
+            .score {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                width: 100%;
+
+                summary {
+
+                    &::-webkit-details-marker, &::marker {
+                        display: none; 
+                        content: "";
+                    }
+
+                    .see-more {
+                        text-decoration: underline;
+                        font-size: .75rem;
+                        color: grey;
+                        margin-bottom: .5rem;
+                    }
+                }
+
+                .set-score {
+                    display: flex;
+                    justify-content: space-evenly;
+                    padding: 0 1rem 1rem 1rem;
+                    gap: 2rem;
+
+                    input {
+                        width: 100px;
+                        height: 3rem;
+                        border: 1px solid lightgray;
+                        font-family: "Tilt Warp", sans-serif;
+                        color: var(--text-color);
+                        border-radius: .5rem;
+                        text-align: center;
+                        font-size: 2rem;
+                        
+                        &::placeholder {
+                            color: grey;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    .babykon-tournament-end {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+
+        .btn {
+            @include btn-primary;
+
+            & {
+                width: 90%;
+                margin: 1rem 0 2rem 0;
+            }
+        }
+    }
+}
 </style>
