9c3bd75a2d2de669489c5b7d684c6b2e1979ac28
Kant109 <quentin.roulet1@gmail.com>
2026-06-17 11:21:52 +0200

    feat: enhance X01 player component with average and throw count display

--- src/components/X01/X01Player.vue
+++ src/components/X01/X01Player.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { averageVolleys, nbThrowDarts } from '@/common/DartsUtils';
 import { useX01GameStore } from '@/stores/X01GameStore';
 import { computed, watch } from 'vue';
 
@@ -10,6 +11,9 @@ const props = defineProps<{
 
 const dartGameStore = useX01GameStore();
 const players = computed(() => dartGameStore.players);
+
+const nbThrowDartsPlayer = computed(() => nbThrowDarts(props.player))
+const averageVolleysPlayer = computed(() => averageVolleys(props.player))
 
 const emit = defineEmits(['isLastPlayer']);
 
@@ -30,18 +34,33 @@ watch(
 <template>
     <div class="full-content" :class="{'top-bg': props.isTopBgActive, 'top-bg-active': props.isTopBgPlayerActive && props.isTopBgActive}">
         <div class="player-content" :class="{'isPlayerActive': player.isActive }">
-            <div class="player-name">{{ player.pseudo }}</div>
+            <div class="top-infos">
+                <div class="player-name">{{ player.pseudo }}</div>
                 <div class="recap">
                     <div class="current-points">
-                    <div class="points" v-if="player.volleys.length > 0 && player.volleys[player.volleys.length - 1][0].includes('O')"><s>{{ player.volleys[player.volleys.length - 1][0].substring(1) }}</s></div>
-                    <div class="points" v-else>{{ player.volleys.length > 0 ? player.volleys[player.volleys.length - 1][0] : "" }}</div>
-                    <div class="points" v-if="player.volleys.length > 0 && player.volleys[player.volleys.length - 1][1].includes('O')"><s>{{ player.volleys[player.volleys.length - 1][1].substring(1) }}</s></div>
-                    <div class="points" v-else>{{ player.volleys.length > 0 ? player.volleys[player.volleys.length - 1][1] : "" }}</div>
-                    <div class="points" v-if="player.volleys.length > 0 && player.volleys[player.volleys.length - 1][2].includes('O')"><s>{{ player.volleys[player.volleys.length - 1][2].substring(1) }}</s></div>
-                    <div class="points" v-else>{{ player.volleys.length > 0 ? player.volleys[player.volleys.length - 1][2] : "" }}</div>
+                        <div class="points" v-if="player.volleys.length > 0 && player.volleys[player.volleys.length - 1][0].includes('O')"><s>{{ player.volleys[player.volleys.length - 1][0].substring(1) }}</s></div>
+                        <div class="points" v-else>{{ player.volleys.length > 0 ? player.volleys[player.volleys.length - 1][0] : "" }}</div>
+                        <div class="points" v-if="player.volleys.length > 0 && player.volleys[player.volleys.length - 1][1].includes('O')"><s>{{ player.volleys[player.volleys.length - 1][1].substring(1) }}</s></div>
+                        <div class="points" v-else>{{ player.volleys.length > 0 ? player.volleys[player.volleys.length - 1][1] : "" }}</div>
+                        <div class="points" v-if="player.volleys.length > 0 && player.volleys[player.volleys.length - 1][2].includes('O')"><s>{{ player.volleys[player.volleys.length - 1][2].substring(1) }}</s></div>
+                        <div class="points" v-else>{{ player.volleys.length > 0 ? player.volleys[player.volleys.length - 1][2] : "" }}</div>
+                    </div>
+                </div>
+                <div class="points-taken">{{ player.points }}</div>
+            </div>
+            <div class="bottom-infos">
+                <div class="darts-details">
+                    <div class="average-volleys">{{ averageVolleysPlayer }} avg.</div>
+                    <div class="nb-throw-darts">
+                        <img src="/public/icons/darts.svg" alt="Darts">
+                        {{ nbThrowDartsPlayer }}
+                    </div>
+                </div>
+                <div class="sets-and-legs">
+                    <div class="sets">{{ player.sets }}</div>
+                    <div class="legs">{{ player.legs }}</div>
                 </div>
             </div>
-            <div class="points-taken">{{ player.points }}</div>
         </div>
     </div>
 </template>
@@ -57,15 +76,8 @@ watch(
 }
 
 .player-content {
-    display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    grid-template-rows: 1fr;
-    grid-column-gap: 0px;
-    grid-row-gap: 0px;
-    height: 60px;
     background-color: var(--bg-color-secondary);
     border-radius: 1rem 1rem 0 0;
-    padding: 0 1rem;
     --tw-shadow: inset 0 5px 0 0 rgba(0, 0, 0, .25);
     box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
 
@@ -77,55 +89,111 @@ watch(
         }
     }
 
-    .player-name, .points-taken {
-        display: flex;
-        align-items: center;
-        font-family: "Tilt Warp", sans-serif;
-        font-size: 1rem;
-        color: var(--text-color);
+    .top-infos {
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        grid-template-rows: 1fr;
+        grid-column-gap: 0px;
+        grid-row-gap: 0px;
+        height: 60px;
+        padding: 0 1rem;
 
-        &:is(.player-name) {
-            min-width: 55px;
-            grid-area: 1 / 1 / 2 / 2;
+        .player-name, .points-taken {
+            display: flex;
+            align-items: center;
+            font-family: "Tilt Warp", sans-serif;
+            font-size: 1rem;
+            color: var(--text-color);
+
+            &:is(.player-name) {
+                min-width: 55px;
+                grid-area: 1 / 1 / 2 / 2;
+            }
+
+            &:is(.points-taken) {
+                justify-content: flex-end;
+                grid-area: 1 / 5 / 2 / 6;
+            }
         }
 
-        &:is(.points-taken) {
-            justify-content: flex-end;
-            grid-area: 1 / 5 / 2 / 6;
+        .recap {
+            display: flex;
+            justify-content: center;
+            grid-area: 1 / 2 / 2 / 5;
+            gap: .5rem;
+            padding-top: 5px;
+
+            .current-points {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                gap: 2rem;
+
+                .points {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    font-family: "Tilt Warp", sans-serif;
+                    font-size: 1rem;
+                    padding-bottom: 5px;
+                    border-radius: 8px;
+                    width: 2.5rem;
+                    aspect-ratio: 1/1;
+                    max-height: 2.5rem;
+                    background-color: white;
+                    border: 1px solid rgba(0, 0, 0, .25);
+                    --tw-shadow: inset 0 -5px 0 0 rgba(0, 0, 0, .25);
+                    box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+
+                    & s {
+                        text-decoration-thickness: 2px;
+                    }
+                }
+            }
         }
     }
 
-    .recap {
+    .bottom-infos {
         display: flex;
-        justify-content: center;
-        flex-direction: column;
-        grid-area: 1 / 2 / 2 / 5;
-        gap: .5rem;
-        padding-top: 5px;
+        justify-content: space-around;
+        border-top: 1px solid rgba(0, 0, 0, .25);
+        padding: 1rem;
 
-        .current-points {
+        .darts-details {
             display: flex;
-            justify-content: center;
-            gap: 2rem;
+            justify-content: space-around;
+            width: 50%;
 
-            .points {
+            .average-volleys, .nb-throw-darts {
                 display: flex;
+                justify-content: space-around;
                 align-items: center;
-                justify-content: center;
                 font-family: "Tilt Warp", sans-serif;
                 font-size: 1rem;
-                padding-bottom: 5px;
-                border-radius: 8px;
-                width: 2.5rem;
-                aspect-ratio: 1/1;
-                background-color: white;
-                border: 1px solid rgba(0, 0, 0, .25);
-                --tw-shadow: inset 0 -5px 0 0 rgba(0, 0, 0, .25);
-                box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+                color: var(--text-color);
 
-                & s {
-                    text-decoration-thickness: 2px;
+                &:is(.nb-throw-darts) {
+                    gap: .5rem;
+        
+                    img {
+                        width: 20px;
+                        height: 20px;
+                    }
                 }
+            }
+        }
+
+        .sets-and-legs {
+            display: flex;
+            justify-content: space-around;
+            width: 50%;
+
+            .sets, .legs {
+                display: flex;
+                align-items: center;
+                font-family: "Tilt Warp", sans-serif;
+                font-size: 1rem;
+                color: var(--text-color);
             }
         }
     }
